9923d0e95b1c3aadd019eb7a3a0e1c0b6fa0d419
Cédric La Barthe <cedric.labarth@gmail.com>
2021-05-30 22:27:32 +0200

    sroll event

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@
 import './App.css';
 
 // Import components
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Profil from './components/Profil';
-// import Navbar from './components/Navbar';
+import Navbar from './components/Navbar';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contacts from './components/Contacts';
@@ -12,11 +12,28 @@ import Intro from './components/Into';
 
 function App() {
   const [intro, setIntro] = useState(true);
+  const [scrollTop, setScrollTop] = useState(false);
+  let prevPos = 0;
+
+  const handleScroll = () => {
+    const topPos = window.pageYOffset || document.documentElement.scrollTop;
+    if (prevPos < topPos) {
+      setScrollTop(false);
+    } else {
+      setScrollTop(true);
+    }
+    console.log(scrollTop, prevPos, topPos);
+    prevPos = topPos <= 0 ? 0 : topPos;
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  }, []);
 
   return (
     <div className="App">
       {intro ? <Intro setIntro={setIntro} /> : null}
-      {/* <Navbar /> */}
+      {scrollTop ? <Navbar /> : null}
       {!intro ? (
         <>
           <Profil />
